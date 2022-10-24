cfa52b36c355de26bb3e75b2dbd69b1d32c1a5d6
Kristofer Martinez <kdmp@protonmail.com>
2022-10-24 11:31:30 -0700

    Complete module 2 project

--- index.js
+++ index.js
@@ -299,11 +299,11 @@ function grade(number) {
   } else if (number < 90 && number >= 80) {
     return `you got a B`;
   } else if (number < 80 && number >= 70) {
-    return `you got a  C`;
+    return `you got a C`;
   } else if (number < 70 && number >= 60) {
     return `you got a D`;
   } else if (number < 60) {
-    return `you got a F`;
+    return `you got an F`;
   }
 }
 
